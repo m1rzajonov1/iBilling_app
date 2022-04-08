6856a944182a726f195e1128a2fb8bc31c67410b
m1rzajonov1 <m1rzajonov1@mail.ru>
2022-04-08 16:19:00 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imkon_exam/screens/contacts_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const ContractsPage(),
     );
   }
 }
--- lib/screens/contacts_page.dart
+++ lib/screens/contacts_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ContractsPage extends StatelessWidget {
-  const ContractsPage({ Key? key }) : super(key: key);
+  const ContractsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: []),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          label: 'Contracts',
+          icon: SvgPicture.asset('assets/images/contracts_activ.svg'),
+        ),
+        BottomNavigationBarItem(
+          label: 'History',
+          icon: SvgPicture.asset('assets/images/history_activ.svg'),
+        ),
+        BottomNavigationBarItem(
+          label: 'New',
+          icon: SvgPicture.asset('assets/images/new_activ.svg'),
+        ),
+        BottomNavigationBarItem(
+          label: 'Saved',
+          icon: SvgPicture.asset('assets/images/saved_activ.svg'),
+        ),
+        BottomNavigationBarItem(
+          label: 'Profile',
+          icon: SvgPicture.asset('assets/images/profile_activ.svg'),
+        )
+      ]),
     );
   }
 }
