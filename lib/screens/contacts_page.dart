e16be904a1e25551085fa45dffd0d6883c62b289
m1rzajonov1 <m1rzajonov1@mail.ru>
2022-04-08 15:49:37 +0500

    first commit

---
+++ lib/screens/contacts_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ContractsPage extends StatelessWidget {
+  const ContractsPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: []),
+    );
+  }
+}
