fbd66f031cc1a3d5a12ca5a03ac68b10b6acc5f7
m1rzajonov1 <m1rzajonov1@mail.ru>
2022-04-09 15:03:46 +0500

    calendar commit

--- lib/screens/contacts_page.dart
+++ lib/screens/contacts_page.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_agenda/calendar_agenda.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:imkon_exam/core/size_config.dart';
@@ -29,6 +30,31 @@ class ContractsPage extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: SvgPicture.asset('assets/images/search.svg'),
           )
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: getHeight(148),
+            width: getWidth(375),
+            child: CalendarAgenda(
+              backgroundColor: const Color(0xFF1E1E20),
+              dateColor: const Color(0xFFD1D1D1),
+              initialDate: DateTime.now(),
+              firstDate: DateTime.now().subtract(Duration(days: 140)),
+              lastDate: DateTime.now().add(Duration(days: 4)),
+              onDateSelected: (date) {
+                print(date);
+              },
+            ),
+          ),
+          SingleChildScrollView(
+              child: Container(
+            margin: EdgeInsets.symmetric(horizontal:getWidth(16)),
+            height: getHeight(459),
+            width: getWidth(343),
+            color: Colors.orange,
+          ))
         ],
       ),
       bottomNavigationBar: myBottomBar(),
